91955d831f8f6fef78bcfe484d416e7ee9621bd5
phenning <phenning@9855b078-7087-11e0-8b26-2f3a526cf534>
2013-04-04 04:06:53 +0000

    git-svn-id: http://medialab-karlsruhe.dyndns.org/repos/FHEM/branches/ntruchsess@250 9855b078-7087-11e0-8b26-2f3a526cf534

--- fhem/FHEM/11_OWX_SER.pm
+++ fhem/FHEM/11_OWX_SER.pm
@@ -147,7 +147,7 @@ sub Detect () {
       $ress .= "master DS2480 re-detected";
       $interface="DS2480";
       $ret=1;
-    } elsif( ($res eq "\x17\x0A\x5B\x0F\x02") || ($res eq "\x00\x17\x0A\x5B\x0F\x02") || ($res eq "\x30\xf8\x00") ){
+    } elsif( ($res eq "\x17\x0A\x5B\x0F\x02") || ($res eq "\x00\x17\x0A\x5B\x0F\x02") || ($res eq "\x30\xf8\x00") || ($res eq "\x06\x00\x09\x07\x80")){
       $ress .= "passive DS9097 detected";
       $interface="DS9097";
       $ret=1;
