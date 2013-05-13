0e5b09e7c52a14b99ee233c7f3ceb6f7a3bb83fe
ntruchsess <norbert.truchsess@t-online.de>
2013-05-13 22:06:39 +0200

    delete everything except refactores OWX-files

--- fhem/rebootcuno.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect 
-
-set timeout 20 
-#set name [lindex $argv 0] 
-#set user [lindex $argv 1] 
-#set password [lindex $argv 2] 
-spawn telnet 192.168.0.91 2323
-expect "Escape character is '^]'."
-send "B"
